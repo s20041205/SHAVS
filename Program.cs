8f7d3d07e55b5dc046c6bd59ec295992764eecd8
s20041205 <stlinalice@gmail.com>
2022-03-03 17:27:18 +0800

    Update files

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Biosensetek.VitalSign.Security;
+
+namespace VitalSignTestCaseValidate
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            TestCaseMgr mgr = new TestCaseMgr();
+            //mgr.DisplayHelp();
+            //for test
+            args = new string[3];
+            args[0] = "19";
+            args[1] = "-crc";
+            args[2] = @"C:\VitalData\[VT-Trace]\c4de9ea95c12fef0dd776fd3070a0cc40e\33d4b8ea60756951806481cc208e732fc3\4e5859816d2cf3421fdf90901e01838dc5.vts";
+            //
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Argument required!");
+                mgr.DisplayHelp();
+            }
+            if (args.Length > 0)
+            {
+                if (args.Length == 1)
+                {
+                    if ("h".Equals(args[0].ToLower()) || "-h".Equals(args[0].ToLower()))
+                    {
+                        mgr.DisplayHelp();
+                    }
+                    else if ("19".Equals(args[0].ToLower()))
+                    {
+                        mgr.DisplayCYB19Help();
+                    }
+                }
+                else if (args.Length == 2)
+                {
+                    string itm = args[0];
+                    string key = args[1];
+                    //
+                    switch (itm)
+                    {
+                        case "19":
+                            if ("-h".Equals(key.ToLower()))
+                            {
+                                mgr.DisplayCYB19Help();
+                            }
+                            break;
+                    }
+                }
+                else if (args.Length == 3)
+                {
+                    string itm = args[0];
+                    string key = args[1];
+                    string str = args[2];
+                    if ("19".Equals(itm.ToLower()) && "-sha1".Equals(key.ToLower()))//not using
+                    {
+                        string encryp = mgr.GetSHA1Hash(str);
+                        Console.WriteLine($"\n[Calculate SHA-1]");
+                        Console.WriteLine($"-sha1 {str} \n=> {encryp}");
+                    }
+                    else if ("19".Equals(itm.ToLower()) && "-fhash".Equals(key.ToLower()))
+                    {
+                        string encryp = FileHashProvider.GetFileHash(str);
+                        Console.WriteLine($"\n[Calculate file hash (SHA-1)]");
+                        Console.WriteLine($"-fhash {str} \n=> {encryp}");
+                    }
+                    else if ("19".Equals(itm.ToLower()) && "-folhash".Equals(key.ToLower()))
+                    {
+                        List<string> lst = new List<string>();
+                        string[] fileEntries = Directory.GetFiles(str, "*");
+                        for (int i = 0; i < fileEntries.Length; i++)
+                        {
+                            string f = Path.GetFileName(fileEntries[i]);
+                            //Console.WriteLine("adding:" + f);
+                            lst.Add(f);
+                        }
+                        string encryp = FileHashProvider.GetFilesHash(str, lst);
+                        Console.WriteLine($"\n[Calculate folder contents hash (SHA-1)]");
+                        Console.WriteLine($"-folhash {str} \n=> {encryp}");
+                    }
+                    else if ("19".Equals(itm.ToLower()) && "-sha512".Equals(key.ToLower()))
+                    {
+                        string encryp = mgr.GetSHA512Hash(str);
+                        Console.WriteLine($"\n[Calculate SHA-512]");
+                        Console.WriteLine($"-sha512 {str} \n=> {encryp}");
+                    }
+                    else if ("19".Equals(itm.ToLower()) && "-crc".Equals(key.ToLower()))
+                    {
+                        byte ckcsum = mgr.CalculateCheckSum(str);
+                        Console.WriteLine($"\n[Calculate CRC checksum]");
+                        string strCRC = string.Format("{0:X}", ckcsum);
+                        Console.WriteLine($"-crc {str} \n=> {strCRC}");
+                    }
+                }
+                else if (args.Length == 4)
+                {
+                    string itm = args[0];
+                    string key = args[1];
+                    string input1 = args[2];
+                    string input2 = args[3];
+                    if ("19".Equals(itm.ToLower()) && "-comp".Equals(key.ToLower()))
+                    {
+                        bool isSame = input1.Equals(input2);
+                        Console.WriteLine($"{input1} vs. {input2} => {isSame}");
+                    }
+                }
+            }
+            
+        }
+        
+    }
+}
---
+++ TestCaseMgr.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using Biosensetek.VitalSign.Persist;
+using Biosensetek.VitalSign.Security;
+using RetrieveFile = Biosensetek.VitalSign.Persist.RetrieveHeader;
+
+namespace VitalSignTestCaseValidate
+{
+    public class TestCaseMgr
+    {
+
+        public void DisplayHelp()
+        {
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("Choice test item #:");
+            Console.WriteLine("06) DMG_UTC06\t07) LMT_UTC07\t08) REG_UTC08");
+            Console.WriteLine("11) TPR_UTC11\t12) RTV_UTC12\t13) EVT_UTC13");
+            Console.WriteLine("18) CYB_UTC18\t19) CYB_UTC19\t");
+            Console.WriteLine("*****************************************************");
+            //Console.ReadLine();
+        }
+        public void DisplayCYB19Help()
+        {
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("19) CYB_UTC19");
+            //Console.WriteLine(" -sha1 (input)\t\t: calculate string SHA-1");
+            Console.WriteLine(" -fhash (path)\t\t : calculate file hash value (SHA-1)");
+            Console.WriteLine(" -folhash (path)\t : calculate folder contents hash value (SHA-1)");
+            Console.WriteLine(" -sha512 (input)\t : calculate string SHA-512");
+            Console.WriteLine(" -comp (input1) (input2) : compare");
+            Console.WriteLine(" -crc (path)\t\t : get file CRC checksum");
+            Console.WriteLine("*****************************************************");
+        }
+        public string GetSHA1Hash(string str)
+        {
+            string hash = string.Empty;
+            try
+            {
+                using (var sha1 = new SHA1Managed())
+                {
+                    hash = BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(str)));
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            hash = hash.Replace("-", "").ToLower();
+            return hash;
+        }
+        public static string GetSHA1Hash(byte[] input)
+        {
+            try
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    var hash = sha1.ComputeHash(input);
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        // can be "x2" if you want lowercase
+                        sb.Append(b.ToString("x2"));
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+        public string GetSHA512Hash(string str)
+        {
+            string hash = string.Empty;
+            try
+            {
+                using (var sha512 = new SHA512CryptoServiceProvider())
+                {
+                    hash = BitConverter.ToString(sha512.ComputeHash(Encoding.UTF8.GetBytes(str)));
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            hash = hash.Replace("-", "").ToLower();
+            return hash;
+        }
+        public string GetSHA512Hash(byte[] input)
+        {
+            try
+            {
+                SHA512 sha512 = new SHA512CryptoServiceProvider();
+                var res = sha512.ComputeHash(input);
+                var sb = new StringBuilder(res.Length * 2);
+                foreach (byte b in res)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+        public Tuple<string, bool> GetFilesHash(string root, string admit, string curFile)
+        {
+            string path = root;
+            string hash = string.Empty;
+            bool curFileCheckSuccess = true;
+            Regex rgx1 = new Regex(@"^([a-z0-9]{34})$");
+            if (!rgx1.IsMatch(admit)) return Tuple.Create(hash, curFileCheckSuccess);
+            if (!Directory.Exists(path)) return Tuple.Create(hash, curFileCheckSuccess);
+            //
+            List<string> lst = new List<string>();
+            string[] fileEntries = Directory.GetFiles(path, "*");
+            for (int i = 0; i < fileEntries.Length; i++)
+            {
+                if (fileEntries[i].EndsWith($".{admit}")) continue;
+                if (File.Exists($"{path}\\{curFile}.vts"))
+                {
+                    if (fileEntries[i].EndsWith(".event"))
+                        continue;
+                    else if (fileEntries[i].Contains($"{curFile}.vts"))
+                    {
+                        if (fileEntries[i].EndsWith(".xyz")) continue;
+                        bool theSame = CompareFileCheckSum(fileEntries[i]);
+                        //Debug($"{fileEntries[i]} checksum={theSame}");
+                        curFileCheckSuccess &= theSame;
+                        continue;
+                    }
+                }
+                string f = Path.GetFileName(fileEntries[i]);
+                //Debug("adding:" + f);
+                lst.Add(f);
+            }
+            if (lst.Count > 0)
+                hash = FileHashProvider.GetFilesHash(path, lst);
+            //Debug($"@FileChainMgr.GetFilesHash: lst.Count={lst.Count} =>>> hash={hash}");
+            return Tuple.Create(hash, curFileCheckSuccess);
+        }
+        private bool CompareFileCheckSum(string fn)
+        {
+            //get checksum in header
+            byte[] boundary = RetrieveHeaderMgr.GetHeader(fn).boundary;
+            if (boundary == null || boundary.Length < 1)
+                return true;
+            //calculate file total bytes (w/o header) and get LSB byte
+            byte byteCkSum = boundary[boundary.Length - 1];
+            byte byteSum = CalculateCheckSum(fn, 200);
+            //
+            /*bool compare = byteCkSum.Equals(byteSum);
+            string hexCkSum = string.Format("{0:X}", byteCkSum);
+            string hexRes = string.Format("{0:X}", byteSum);
+            Debug($"file LSB = {hexRes}, checkSum={hexCkSum} => check:{success}");*/
+            return byteCkSum.Equals(byteSum);
+        }
+        public byte CalculateCheckSum(string fn, int header = 200)
+        {
+            uint len = 0;
+            if (!File.Exists(fn)) return 0;
+            FileInfo f = new FileInfo(fn);
+            if (f.Length > 200)
+                len = (uint)f.Length - 200;
+            else
+                return 0;
+            //
+            byte[] tmpRaw = new byte[len];
+            ulong tmp = 0;
+            try
+            {
+                using (FileStream fs = new FileStream(fn, FileMode.Open))
+                {
+                    using (BinaryReader br = new BinaryReader(fs))
+                    {
+                        try
+                        {
+                            fs.Seek(header, SeekOrigin.Begin);
+                            br.Read(tmpRaw, 0, tmpRaw.Length);
+                        }
+                        catch (Exception ex) { System.Diagnostics.Debug.Print(ex.Message); }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Debug("@CalculateCheckSum exception:" + ex);
+            }
+            for (int i = 0; i < tmpRaw.Length; i++)
+            {
+                tmp += tmpRaw[i];
+                if (tmp >= Int32.MaxValue / 2)
+                    tmp = tmp & 0x00FF;
+            }
+            tmp = (uint)tmp & 0x00FF;
+
+            return Convert.ToByte(tmp);
+        }
+    }
+}
